f301da7d88d8f9f5902d0ad97f4f00c6876dec81
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-10-19 22:07:08 -0400

    started adding bulma styling to signup/login

--- src/components/ShowSignup.jsx
+++ src/components/ShowSignup.jsx
@@ -15,7 +15,8 @@ class ShowSignup extends Component {
 
   render() {
     return (
-      <div className="user-box">
+    <section className="section">
+      <div className="container">
         <form
           className="login-box"
         onSubmit={
@@ -26,42 +27,60 @@ class ShowSignup extends Component {
         }
         >
           <h1>Login</h1>
-          <label htmlFor="login-username">username:</label>
-          <input
-            id="login-username"
-            type="text"
-            onChange={(event) => {
-              this.setState({loginUsername: event.target.value})
-            }}
-          />
-          <br />
-          <label htmlFor="login-password">password:</label>
-          <input id="login-password" type="password" onChange={(event) => this.setState({loginPassword: event.target.value})} />
-          <br />
+          <div className="field">
+            <label htmlFor="login-username" className="label">username:</label>
+            <div className="control">
+              <input
+                id="login-username"
+                type="text"
+                onChange={(event) => {
+                  this.setState({loginUsername: event.target.value})
+                }}
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label htmlFor="login-password" className="label">password:</label>
+            <div class="control">
+              <input id="login-password" type="password" onChange={(event) => this.setState({loginPassword: event.target.value})} />
+            </div>
+          </div>
           <button>Login</button>
         </form>
-        <form
-          className="signup-box"
-          onSubmit={
-            (event) =>  {
-              event.preventDefault();
-              this.props.onSignup({"username": this.state.signupUsername, "name": this.state.signupName, "password": this.state.signupPassword})
+        </div>
+        <div class="container">
+          <form
+            className="signup-box"
+            onSubmit={
+              (event) =>  {
+                event.preventDefault();
+                this.props.onSignup({"username": this.state.signupUsername, "name": this.state.signupName, "password": this.state.signupPassword})
+              }
             }
-          }
-          >
-          <h1>Signup</h1>
-          <label htmlFor="signup-username">username:</label>
-          <input id="signup-username" type="text" maxlength="10" onChange={(event) => this.setState({signupUsername: event.target.value})} />
-          <br />
-          <label htmlFor="signup-name">name:</label>
-          <input id="signup-name" type="text" onChange={(event) => this.setState({signupName: event.target.value})} />
-          <br />
-          <label htmlFor="signup-password">password:</label>
-          <input id="signup-password" type="password" onChange={(event) => this.setState({signupPassword: event.target.value})} />
-          <br />
-          <button>Signup</button>
-        </form>
-      </div>
+            >
+            <h1>Signup</h1>
+            <div className="field">
+              <label htmlFor="signup-username" className="label">username:</label>
+              <div class="control">
+                <input className="input" id="signup-username" type="text" maxlength="10" onChange={(event) => this.setState({signupUsername: event.target.value})} />
+              </div>
+            </div>
+            <div className="field">
+              <label htmlFor="signup-name" className="label">name:</label>
+              <div class="control">
+                <input className="input" id="signup-name" type="text" onChange={(event) => this.setState({signupName: event.target.value})} />
+              </div>
+            </div>
+            <div className="field">
+              <label htmlFor="signup-password" className="label">password:</label>
+              <div class="control">
+                <input className="input" id="signup-password" type="password" onChange={(event) => this.setState({signupPassword: event.target.value})} />
+              </div>
+            </div>
+            <button>Signup</button>
+          </form>
+        </div>
+      </section>
     );
   }
 }
