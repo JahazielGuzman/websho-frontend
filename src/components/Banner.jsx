dbcc8640262c91f9df135cef0646711c2ada1eb9
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-12-21 13:00:19 -0500

    put search and user name and login button in a div so that we have a hamburger on smaller screens

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -10,24 +10,26 @@ class Banner extends Component {
   render() {
     return (
       <div className="banner">
-        <div className="logo" onClick={this.props.showHome} >WebSho</div>
-        <SearchBar key={this.props.showLogin} onSearchSubmit={this.props.onSearchSubmit} />
-        <div className="user-panel">
-          <span className="user-name">
-            {this.props.user ? this.props.user.username : ''}
-          </span>
-          <span className="session-button">
-            {
-              (this.props.showLogin) ? "" :
-                (this.props.user) ?
-                <button className="session-change" onClick={this.props.onClickSignOut}>
-                  Sign Out
-                </button>:
-                <button className="session-change" onClick={this.props.onClickSignIn}>
-                  Sign In
-                </button>
-            }
-          </span>
+        <div className="logo" onClick={this.props.showHome}>WebSho</div>
+        <div class="hamburger">
+          <SearchBar key={this.props.showLogin} onSearchSubmit={this.props.onSearchSubmit} />
+          <div className="user-panel">
+            <span className="user-name">
+              {this.props.user ? this.props.user.username : ''}
+            </span>
+            <span className="session-button">
+              {
+                (this.props.showLogin) ? "" :
+                  (this.props.user) ?
+                  <button className="session-change" onClick={this.props.onClickSignOut}>
+                    Sign Out
+                  </button>:
+                  <button className="session-change" onClick={this.props.onClickSignIn}>
+                    Sign In
+                  </button>
+              }
+            </span>
+          </div>
         </div>
       </div>
     );
